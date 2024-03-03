4c61f2ea0110a0ffce55addf4b3d18d944a7cee9
mlinares <m.linares93@gmail.com>
2024-03-02 22:27:57 -0500

    create notes index url, view, and template

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Note
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def notes_index(request):
+  notes = Note.objects.all()
+  return render(request, 'notes/index.html', { 'notes': notes })
