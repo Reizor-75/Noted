df3bbea7da90e1b60902200fee1c24a0a3068617
mlinares <m.linares93@gmail.com>
2024-03-02 20:50:28 -0500

    set up note Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Note(models.Model):
+  title: models.CharField(max_length=100)
+  subject: models.CharField(max_length=100)
+  date: models.DateField("Creation Date")
+  key: models.CharField(max_length=100)
+  content: models.TextField(max_length=1000)
+  summary: models.TextField(max_length=1000)  
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.Title} "
