d6afae09ad6db3fa6c2352df60d90772947cb5c9
mlinares <m.linares93@gmail.com>
2024-03-07 12:57:31 -0500

    fix bug in change theme

--- main_app/context_processor.py
+++ main_app/context_processor.py
@@ -3,13 +3,19 @@ from .models import Theme
 def set_theme(request):
   if request.user.is_authenticated:
     cur_theme = Theme.objects.filter(user = request.user).last()
-    return { 'THEME': cur_theme.color }
+    if(cur_theme):
+      return { 'THEME': cur_theme.color }
+    else:
+      return { 'THEME': "NA" }
   else:
     return { 'THEME': "NA" }
 
 def set_font(request):
   if request.user.is_authenticated:
     cur_theme = Theme.objects.filter(user = request.user).last()
-    return { 'FONT': cur_theme.font }
+    if(cur_theme):
+      return { 'FONT': cur_theme.font }
+    else:
+      return { 'FONT': "1" }
   else:
     return { 'FONT': "1" }
